a6953e8b342a2c0c7baa10006e17b7351b15a797
Nikita <herbby4@gmail.com>
2021-10-15 08:16:26 +0300

    Add sh docker-compose

---
+++ docker-compose.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
